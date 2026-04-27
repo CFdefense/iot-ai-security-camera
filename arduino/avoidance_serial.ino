f0ae379f6b6d6533972226d9c45ba7494c6ee32f
jfrank03 <jfrank@vassar.edu>
2026-04-27 11:39:23 -0400

    Added code to send detection events and a heartbeat through serial.

--- arduino/avoidance_serial.ino
+++ arduino/avoidance_serial.ino
@@ -1,12 +1,20 @@
+// Using avoidance sensor OR tracking sensor...
+// SEN -> ARD:
+// GND -> GND
+//  +  -> 5V
+// S/OUT -> Digital 2
+
 const int PIN_AVOID = 2;
 const int OBSTACLE_ACTIVE = LOW;   // change to HIGH if your sensor is inverted
 const int STABLE_COUNT_REQUIRED = 3;
 const unsigned long PERIOD_MS = 100;
+const unsigned long HEARTBEAT_PERIOD_MS = 5000;   // Arduino sends every 5 seconds
 const char* DEVICE_ID = "frank-arduino-01";
 const char* RULE_NAME = "avoidance_hysteresis_v1";
 
 unsigned long event_id = 0;
 unsigned long last_sample_ms = 0;
+unsigned long last_heartbeat_ms = 0;
 
 bool confirmed_blocked = false;
 int last_raw = -1;
@@ -50,5 +58,21 @@ void loop() {
     Serial.print(RULE_NAME);
     Serial.print("\"}");
     Serial.println();
+  } else {
+    // Send heartbeat if no state change, to indicate we're alive
+    if (now - last_heartbeat_ms >= HEARTBEAT_PERIOD_MS) {
+      Serial.print("{\"device_id\":\"");
+      Serial.print(DEVICE_ID);
+      Serial.print("\",\"ts_ms\":");
+      Serial.print(now);
+      Serial.print(",\"event_type\":\"heartbeat\"");
+      Serial.print(",\"sensor\":\"avoidance\"");
+      Serial.print(",\"status\":\"alive\"");
+      Serial.print(",\"value\":");
+      Serial.print(confirmed_blocked ? 1 : 0);
+      Serial.print("}");
+      Serial.println();
+      last_heartbeat_ms = now;
+    }
   }
 }
